80aa3e214f856ca852a836ea22a2100b0951cb2f
Alexander Zagniotov <azagniot@thoughtworks.com>
2011-10-06 10:10:45 +1100

    Added footer information as UI template, UI tweaks

--- public/javascripts/UIFooterTemplate.js
+++ public/javascripts/UIFooterTemplate.js
@@ -1,23 +1,23 @@
 (function() {
-  window.UIFooterTemplate = '\
-<div id="footer"><p>For changes or cancellations outside business hours please contact airlines and drivers directly<br />\
-via their website or the following numbers.</p>\
+  window.UIFooterTemplate = '<div id="itenary-footer">\
+<p>-----------------------------------------------------------------------<br /></p>\
+<p>For changes or cancellations outside business hours please contact airlines and drivers directly<br />\
+via their website or the following numbers.<br /></p>\
 \
-<p>\
-<b>AIRLINES</b>\
+<p><b>AIRLINES</b><br />\
 Qantas 13 13 13<br />\
 Virgin Blue 13 67 89<br /><br />\
 \
-<b>CAR SERVICES</b>\
+<b>CAR SERVICES</b><br />\
 Melbourne 0412 932 628<br />\
 Sydney 0413 333 663<br />\
 Perth 0488 366 042<br />\
-Brisbane 07 3353 0644</p>\
+Brisbane 07 3353 0644<br /></p>\
 \
 <p>When travelling for ThoughtWorks you are covered by <b>CoverMore Travel Insurance</b><br />\
 Account Name:ThoughtWorks Australia Pty Ltd<br />\
-Policy Number:5361054</p>\
-<p>\
-Got Feedback? How was your travel today? Were the timings appropriate? Did everything go smoothly?<br />\
+Policy Number:5361054<br /></p>\
+\
+<p>Got Feedback? How was your travel today? Were the timings appropriate? Did everything go smoothly?<br />\
 Please let Jacqui know of any feedback you have to help us provide the best travel service we can.</p></div>';
 }).call(this);
