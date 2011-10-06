1a7a699d1b0020505181dc4aab061ad242b6edac
Alexander Zagniotov <azagniot@thoughtworks.com>
2011-10-06 17:00:26 +1100

    UI tweaks

--- public/javascripts/UICarTemplate.js
+++ public/javascripts/UICarTemplate.js
@@ -1,6 +1,18 @@
 (function() {
   window.UICarTemplate = '\
-<table border="0" cellpadding="0" cellspacing="0">\
+\
+<style type="text/css">\
+table tr th {\
+    color: #000 !important;\
+    font-weight: bold !important;\
+    height: 2em !important;\
+    padding: 2px 4px !important;\
+    text-align: left !important;\
+    white-space: nowrap !important;                                                                                                                                               \
+}\
+</style>\
+\
+<table border="0" cellpadding="5px" cellspacing="5px">\
 	<tr>\
         <th>City</th><th>Company</th><th>Contact</th><th>Phone</th>\
     </tr>\
@@ -9,5 +21,6 @@
 		<td>{{city}}</td><td>{{company}}</td><td>{{contact}}</td><td>{{phone}}</td>\
 	</tr>\
 	{{/cars}}\
-</table>';
+</table>\
+';
 }).call(this);
