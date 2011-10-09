06dad829804c65bcafa0aa9ddb649869cc2c97d3
Alexander Zagniotov <azagniot@thoughtworks.com>
2011-10-09 17:49:28 +1100

    Playing with Ajax and Google distance API - UI tweaks

--- public/javascripts/2.google.map.lib.loader.js
+++ public/javascripts/2.google.map.lib.loader.js
@@ -4,9 +4,8 @@ var newScript1 = document.createElement('script');
 newScript1.type = 'text/javascript';
 newScript1.src = "http://maps.googleapis.com/maps/api/js?sensor=false";
 
-var newScript2 = document.createElement('script');                                                                                                                                
-newScript2.type = 'text/javascript';
-newScript2.src = "http://maps.gstatic.com/intl/en_us/mapfiles/api-3/6/7/main.js";
+//var newScript2 = document.createElement('script');                                                                                                                              //newScript2.type = 'text/javascript';
+//newScript2.src = "http://maps.gstatic.com/intl/en_us/mapfiles/api-3/6/7/main.js";
    
 headID.appendChild(newScript1);
-headID.appendChild(newScript2);
+//headID.appendChild(newScript2);
